0774b8e968954b7d0785ce33c7ebb1006302a16e
Sebastian Cascante <139644093+devSebaKsk@users.noreply.github.com>
2025-02-24 03:09:38 +0000

    Semaforo

---
+++ src/js/components/Semaforo.jsx
@@ -0,0 +1,18 @@
+import React, {useState} from 'react'
+
+import '/src/styles/index.css'
+
+function Semaforo() {
+  const [seleccionado, setSeleccionado] = useState("ligth-on");
+  return (
+    <div className='cuerpo'>
+				<div className={`bg-danger ${seleccionado == 'bg-danger' ? 'ligth-on' : ''}`} onClick={() => { setSeleccionado("bg-danger") }}></div>
+
+				<div className={`bg-warning ${seleccionado == 'bg-warning' ? 'ligth-on' : ''}`} onClick={() => { setSeleccionado("bg-warning") }}></div>
+
+				<div className={`bg-success ${seleccionado == 'bg-success' ? 'ligth-on' : ''}`} onClick={() => { setSeleccionado("bg-success") }}></div>
+			</div>
+  )
+}
+
+export default Semaforo
