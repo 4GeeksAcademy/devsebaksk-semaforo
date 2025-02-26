65de6a000b264f7483df6473ffbbddb66194b9de
Sebastian Cascante <139644093+devSebaKsk@users.noreply.github.com>
2025-02-26 10:22:46 +0000

    Mejorar visual

--- src/js/components/Semaforo.jsx
+++ src/js/components/Semaforo.jsx
@@ -5,6 +5,8 @@ import '/src/styles/index.css'
 function Semaforo() {
   const [seleccionado, setSeleccionado] = useState("ligth-on");
   return (
+    <div className='contenedor'>
+      <div className='soporte'></div>
     <div className='cuerpo'>
 				<div className={`bg-danger ${seleccionado == 'bg-danger' ? 'ligth-on' : ''}`} onClick={() => { setSeleccionado("bg-danger") }}></div>
 
@@ -12,6 +14,7 @@ function Semaforo() {
 
 				<div className={`bg-success ${seleccionado == 'bg-success' ? 'ligth-on' : ''}`} onClick={() => { setSeleccionado("bg-success") }}></div>
 			</div>
+      </div>
   )
 }
 
